5fd309c7bfa49e8e64eadeec1f1a98bb002cdc81
Mohammad Arsalan Rajput <a4arshi@yahoo.com>
2016-01-13 09:12:49 +0500

    todo app

---
+++ app/app.ts
@@ -0,0 +1,51 @@
+/// <reference path="../typings/tsd.d.ts" />
+angular
+    .module("todoApp",["ngMaterial","ngMdIcons"])
+    // .controller("todoController" , ["todoService" ,"$timeout", "$mdSidenav, "$log", todoController])
+    // function todoController(todoService,$timeout, $mdSidenav, $log) {
+    //     var self = this;
+    //     this.todo =  todoService.arr
+    // }
+  .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      $mdSidenav('right').close()
+        .then(function () {
+          $log.debug("close RIGHT is done");
+        });
+    };
+  })
+    .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.toggleRight = buildToggler('right');
+    $scope.isOpenRight = function(){
+      return $mdSidenav('right').isOpen();
+    };
+    
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+  
+    function buildToggler(navID) {
+      return function() {
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      }
+    }
+  })
+ 
+  
---
+++ app/todoservice.ts
@@ -0,0 +1,20 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+angular 
+      .module("todoApp")
+      .service("todoService" ,function(){
+         var self = this;
+        this.todoArray = [
+            {
+                name : "Arsalan" , 
+                age:  23            
+            }
+        ];
+        this.addItem = function(){
+            self.todoArray.push()
+        }
+        this.removeItem = function(index){
+            self.todoArray.splice(index ,1)
+        }
+        return this.todoArray;
+      })
